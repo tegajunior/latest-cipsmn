4390b13911c9a5110e115becf3cfb7b9737e362f
tegajunior <chidiebereuzomahumble@gmail.com>
2022-03-21 11:07:38 +0100

    initial file upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment, useContext } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import AuthContext from "./store/auth-context";
+import Signup from "./pages/users/Signup";
+import ListUsers from "./pages/admin/ListUsers";
+import Login from "./pages/common/Login";
+import ViewUser from "./pages/admin/ViewUser";
+import AdminDashboard from "./pages/admin/Dashboard";
+import UserDashboard from "./pages/users/Dashboard";
+import AdminRegisterUser from "./pages/admin/AdminRegisterUser";
+import UserCerts from './pages/users/Certs'
 
 function App() {
+  // let url = '/app.bundle.js';
+  // useEffect(() => {
+  //   const scriptFile = document.createElement('script');
+  //   scriptFile.type = 'text/javascript';
+  //   scriptFile.src = url;
+  //   scriptFile.async = true;
+  //   document.body.appendChild(scriptFile);
+  // }, [url]);
+  const authCtx = useContext(AuthContext)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Switch>
+        <Route path="/" exact>
+          <Login />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+        <Route path="/admin" exact>
+          {authCtx.isLoggedIn && <AdminDashboard />}
+          {!authCtx.isLoggedIn && <Redirect to="/login" />}
+        </Route>
+        <Route path="/admin/register-user">
+          {authCtx.isLoggedIn && <AdminRegisterUser />}
+          {!authCtx.isLoggedIn && <Redirect to="/login" />}
+        </Route>
+        <Route path="/admin/list-users" exact>
+          {authCtx.isLoggedIn && <ListUsers />}
+          {!authCtx.isLoggedIn && <Redirect to="/login" />}
+        </Route>
+        <Route path="/admin/view-user/:id" exact>
+        {authCtx.isLoggedIn && <ViewUser />}
+        {!authCtx.isLoggedIn && <Redirect to="/login" />}
+        </Route>
+        <Route path="/user" exact>
+          {authCtx.isLoggedIn && <UserDashboard />}
+          {!authCtx.isLoggedIn && <Redirect to="/login" />}
+        </Route>
+        <Route path="/user/certs" exact>
+          {authCtx.isLoggedIn && <UserCerts />}
+          {!authCtx.isLoggedIn && <Redirect to="/login" />}
+        </Route>
+      </Switch>
+    </Fragment>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import { AuthContextProvider } from './store/auth-context';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <AuthContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AuthContextProvider>,
   document.getElementById('root')
 );
 
